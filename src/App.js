8cb8d75ce0f1f908a3137b701e50d2ee52ed493e
oleg dedyulya <zzoleg@gmail.com>
2019-01-13 06:03:10 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//import Button from 'react-bootstrap/lib/Button'; //вставляем Button без фигурных скобок так как подянеться весь комплект что повлияет на скорость
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,9 +26,36 @@ class App extends Component {
             Learn React
           </a>
         </header>
-      </div>
+      </div>*/
+    <div className="Wrapper">
+			<SayFullName name="Oleg" surname="Ded" link="vk.com" />
+			<SayFullName name="Dima" surname="Ded" link="@" />
+			<SayFullName name="Yulya" surname="Nosenk" link="facebook.com" />
+
+		</div>
+      
     );
   }
 }
 
+
+function SayFullName(props) {
+	return (
+		<div>
+		<h1>Моё имя {props.name}, фамилия - {props.surname}</h1>
+		<a href={props.link}>Ссылка на мой профиль</a>
+		</div>
+	)
+}
+
+/*function MeetAll () {
+	return (
+		<div>
+			<SayFullName name="Oleg" surname="Ded" link="vk.com" />
+			<SayFullName name="Dima" surname="Ded" link="@" />
+			<SayFullName name="Yulya" surname="Nosenk" link="facebook.com" />
+
+		</div>
+	)
+}*/
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,42 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+
+/*function Hello() {
+	let phrase = "World!"
+	return (
+			<h1>Hello {phrase} kiss {4+10}</h1>
+		)
+}
+
+function SayFullName(props) {
+	return (
+		<div>
+		<h1>Моё имя {props.name}, фамилия - {props.surname}</h1>
+		<a href={props.link}>Ссылка на мой профиль</a>
+		</div>
+	)
+}
+
+function MeetAll () {
+	return (
+		<div>
+			<SayFullName name="Oleg" surname="Ded" link="vk.com" />
+			<SayFullName name="Dima" surname="Ded" link="@" />
+			<SayFullName name="Yulya" surname="Nosenk" link="facebook.com" />
+
+		</div>
+	)
+}*/
+
+//const element = <SayFullName name="Oleg" surname="Ded" link="vk.com"  />
+
+//ReactDOM.render(<MeetAll />, document.getElementById('root'));
+//ReactDOM.render(<SayFullName name="Oleg" surname="Ded" link="vk.com"  />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
